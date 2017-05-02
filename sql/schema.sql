aa2ac16cc5d6ab81ec718a643ba6d9ddb82657f4
Kevin Li <github@mail.kevinl.io>
2017-05-02 13:41:32 -0400

    add extra tables and cols in schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -3,6 +3,7 @@ BEGIN;
 DROP TABLE IF EXISTS renewal_item;
 DROP TABLE IF EXISTS renewal;
 DROP TABLE IF EXISTS profile;
+DROP TABLE IF EXISTS notification_setting;
 
 CREATE TABLE profile (
     id SERIAL PRIMARY KEY,
@@ -15,10 +16,30 @@ CREATE TABLE profile (
     -- for alerts
     phone_number TEXT,
     created_at TIMESTAMP NOT NULL DEFAULT ( now() ),
+    -- whether the account has already paid
+    paid BOOLEAN NOT NULL DEFAULT ( false ),
+    -- the time at which the account was paid
+    time_paid TIMESTAMP,
 
     UNIQUE ( username ),
     UNIQUE ( email_address ),
     UNIQUE ( phone_number )
+);
+
+CREATE TABLE notification_setting (
+    id SERIAL PRIMARY KEY,
+    profile_id INTEGER NOT NULL,
+    -- 1-5 representing notification level.
+    -- 1 : all
+    -- 2 : only failure
+    -- 3 : weekly digest
+    -- 4 : monthly digest
+    -- 5 : never
+    notification_level INTEGER NOT NULL,
+
+    CONSTRAINT fk_profile_id__profile_id
+    FOREIGN KEY ( profile_id )
+    REFERENCES profile ( id )
 );
 
 -- a renewal is a batch of renewals that happens at a given time, each of which
